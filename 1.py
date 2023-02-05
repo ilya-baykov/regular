370bda76cafce488db596b6006f2c3bf78629287
ilya-baykov <i-baykov@internet.ru>
2023-02-06 00:12:53 +0300

    Вводные примеры использования регулярных выражений

--- 1.py
+++ 1.py
@@ -3,6 +3,7 @@ import re
 text = "Карта map и объект bitmap - разные вещи"
 text_2 = "еда,победа,беда"
 text_3 = "Еда,победа,беду"
+text_4 = "Мне сейчас 21 год , но уже скоро мне будет22(пишу без пробела)"
 
 match = re.findall(r"\bmap\b", text)
 
@@ -10,7 +11,10 @@ match_2 = re.findall("еда", text_2)
 
 match_3 = re.findall(r"[еЕ]д[ау]", text_3)
 
+match_4 = re.findall("[0123456789]", text_4)
+
 if __name__ == '__main__':
     print(match)
     print(match_2)
     print(match_3)
+    print(match_4)
